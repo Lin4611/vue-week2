c6ac00aa9182a9ff9ee290fbcf3fe943469e54e0
Lin <tzu.hsun.lin046@gmail.com>
2025-08-10 03:46:33 +0800

    update link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hexschool-2025course/',
+  base: '/vue-week2/',
   plugins: [
     vue(),
     vueDevTools(),
